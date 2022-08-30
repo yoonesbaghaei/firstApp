c3a441ae45755e6a388a618a876e2043a53b78e4
yoonesbaghaei <bagha1383@gmail.com>
2022-08-30 13:04:16 +0430

    app test

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/yoonesb/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/yoonesb/projects/test_aap" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/yoonesb/flutter"
+  "PROJECT_DIR=/home/yoonesb/projects/test_aap"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/yoonesb/projects/test_aap/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/yoonesb/projects/test_aap/lib/main.dart"
+)
